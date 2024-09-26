5d9277a507003aef2c55e8d65e76f1d8cd91c326
Luis Henrique <luissecundes@gmail.com>
2024-09-26 08:47:45 -0300

    Exemplo com collection

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/select', function () {
+$users = User::all();
+dd($users);
+});
 
 Route::get('/', function () {
     return view('welcome');
